bc1ef33f59f837ab399b008f56ac3525a34781c7
Lazy-CSharp-Coder <haraldn.kodeloftet@gmail.com>
2025-09-21 18:34:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SaturnKodeloft1
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Lazy-CSharp-Coder/SaturnKodeloft1)
---
+++ script.js
@@ -0,0 +1,17 @@
+function hamburgerToggle() {
+  console.log("Hi from hamburgerToggle");
+
+  const navListElement = document.querySelector(".navList"); // Bevist brukt klasse for kun et element med det klassenavnet
+  console.log(navListElement);
+
+  // Metode 1: Skrive CSS i JS som blir inline-CSS
+  navListElement.style.display = "flex";
+  navListElement.style.flexDirection = "column";
+
+  // Metode 2: Endre aktivt klassenavn via external-CSS fil
+  /*navListElement.classList.add("show");
+  navListElement.classList.remove("hidden");*/
+}
+
+// Gjør funksjonen tilgjengelig i globalt scope slik at den kan brukes i inline onclick-egenskaper.
+//globalThis.hamburgerToggle = hamburgerToggle;
